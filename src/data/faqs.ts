d3a3d3c8a1c27b86ba7287aa1088f073f65a6424
junior31064049@gmail.com <junior31064049@gmail.com>
2024-12-28 15:10:24 -0500

    add about us section and faqs

---
+++ src/data/faqs.ts
@@ -0,0 +1,21 @@
+import { Faq } from "@/types/faq";
+
+export const Faqs: Faq[] = [
+  {
+    id: "1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "2",
+    question: "Is it styled?",
+    answer:
+      "Yes. It comes with default styles that matches the other components' aesthetic.",
+  },
+  {
+    id: "3",
+    question: "Is it animated?",
+    answer:
+      "Yes. It's animated by default, but you can disable it if you prefer.",
+  },
+];
